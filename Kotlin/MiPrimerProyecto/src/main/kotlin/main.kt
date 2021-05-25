d80217357eb78d92f2439ad58ffaa76d6dee39a9
SAW1518 <enriqueppp5@hotmail.com>
2021-05-25 00:45:10 -0500

    Kodlin init

---
+++ Kotlin/MiPrimerProyecto/src/main/kotlin/main.kt
@@ -0,0 +1,27 @@
+const val PI = 3.1419
+fun main(args: Array<String>) {
+    println("Hello World!")
+    var money = 10
+    println(money)
+    money = 5
+    println(money)
+    val name = "Laura"
+    //name = "Kike"
+    println(name)
+    println(PI)
+
+    val boolean = true
+    val numberLarge = 3L
+    val double = 2.7182
+    val float = 1.1f
+
+    val firstValue = 20
+    val secondValue = 10
+    val thirdValue = firstValue - secondValue
+    println(thirdValue)
+
+    val lastName = "Uribe"
+    val myName = "kike"
+    val completeName = "My Name is  $lastName $myName"
+    println(completeName)
+}
