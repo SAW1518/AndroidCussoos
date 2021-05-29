ff87d42178ce2b4480c714bda6739a4e0142ba19
SAW1518 <enriqueppp5@hotmail.com>
2021-05-29 01:25:41 -0500

    update

--- Kotlin/MiPrimerProyecto/src/main/kotlin/main.kt
+++ Kotlin/MiPrimerProyecto/src/main/kotlin/main.kt
@@ -58,7 +58,6 @@ fun main(args: Array<String>) {
         else -> "There is not"
     }
     println(message)*/
-
     /*var accountant = 0
     while (accountant > 0) {
         println("The value is $accountant")
@@ -69,12 +68,104 @@ fun main(args: Array<String>) {
         val number = (0..100).random()
         println("number random.....$number")
     } while (number > 50)*/
+    /* val listFruit = listOf("Apple", "Pear", "Peach", "Mango")
+     for (fruit in listFruit) println("eat $fruit")
+     listFruit.forEach { fruit -> println("forEach eat $fruit") }
+     val charCharacters: List<Int> = listFruit.map { fruit -> fruit.length }
+     println("charCharacters eat $charCharacters")
+     val filterList: List<Int> = charCharacters.filter { char -> char > 4 }
+     println("filterList eat $filterList")*/
+    /*
+    val name: String? = null
+    println(name?.length)
+    try {
+        throw NullPointerException("Errror aocurrdo nulll")
+        name!!.length
+    } catch (excepcion: NullPointerException) {
+        println("Error")
+    } finally {
+        println("Error Close app ")
+    }
+    val firstVal = 10
+    val secondVal = 0
+    val res: Int = try {
+        firstVal / secondVal
+    } catch (exception: Exception) {
+        0
+    }
+    println(res)*/
+/*
+    val name : String? = null
+    val crName : Int = name?.length ?: 0
+    println(crName)
+    */
+    /*
+    //no mutable
+    val listName = listOf("Juan", "Kike", "Cail")
+    println(listName)
 
-    val listFruit = listOf("Apple", "Pear", "Peach", "Mango")
-    for (fruit in listFruit) println("eat $fruit")
-    listFruit.forEach { fruit -> println("forEach eat $fruit") }
-    val charCharacters: List<Int> = listFruit.map { fruit -> fruit.length }
-    println("charCharacters eat $charCharacters")
-    val filterList: List<Int> = charCharacters.filter { char -> char > 4 }
-    println("filterList eat $filterList")
+    val EntryList = mutableListOf<String>()
+    EntryList.add("JUAN")
+    println(EntryList)
+
+    val valGet = EntryList.get(0)
+    println(valGet)
+    val valGetWhitOperator = EntryList[0]
+    println(valGetWhitOperator)
+    val firstValueList: String? = EntryList.firstOrNull()
+    println(firstValueList)
+    EntryList.removeAt(0)
+    println(EntryList)
+    EntryList.add("Enrique")
+    EntryList.removeIf { char -> char.length > 3 }
+    println(EntryList)
+    val  myArray = arrayListOf(1,2,3,4)
+    //BIT CODE
+    println("Array $myArray")
+    //To list
+    println("Array ${myArray.toList()}")*/
+/*
+    // Order List
+    val numbers = listOf(132321, 232, 265, 456, 524)
+    val sorted = numbers.sorted()
+    val sortedDescending = numbers.sortedDescending()
+    val sortedBy = numbers.sortedBy { number -> number < 50 }
+    val sortedRandom = numbers.shuffled()
+    val sortedReverse = numbers.reversed()
+    val numbersMap = numbers.map { number -> "number $number" }
+    val numbersFilter = numbers.filter { number -> number > 400 }
+    println(sorted)
+    println(sortedDescending)
+    println(sortedBy)
+    println(sortedRandom)
+    println(sortedReverse)
+    println(numbersMap)
+    println(numbersFilter)
+*/
+
+/*
+    //NOT mutable
+    val ageHero = mapOf(
+        "IronMan" to 35,
+        "SpiderMan" to 23,
+        "Capitan America" to 99,
+    )
+    println(ageHero)
+//YES mutable
+    val ageHeroMutable = mutableMapOf(
+        "IronMan" to 35,
+        "SpiderMan" to 23,
+        "Capitan America" to 99,
+    )
+    println(ageHeroMutable)
+    ageHeroMutable["Wonder Woman"] = 45 //or ageHeroMutable.put("Wonder Woman", 45)
+    println(ageHeroMutable)
+    val ageIronMan = ageHeroMutable["IronMan"]
+    println(ageIronMan)
+    ageHeroMutable.remove("Wonder Woman")
+    println(ageHeroMutable)
+    println(ageHeroMutable.keys)
+    println(ageHeroMutable.values)*/
+
+
 }
